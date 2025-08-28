52be613b5406dc555339da65651c1302e2b7621f
Russel Arbore <russel.jma@gmail.com>
2025-08-28 11:20:43 -0700

    Rem's algorithm for UF

--- util/src/union_find.rs
+++ util/src/union_find.rs
@@ -1,6 +1,6 @@
 use crate::vec::{VirtualVec};
 
-#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct ClassId(u32);
 
 pub struct UnionFind {
@@ -21,22 +21,45 @@ impl UnionFind {
         id
     }
 
-    pub fn find(&self, mut id: ClassId) -> ClassId {
-        loop {
-            let parent = self.vec[id.0 as usize];
-            if parent == id {
-                return id;
-            } else {
-                id = parent;
-            }
+    pub fn find(&mut self, mut id: ClassId) -> ClassId {
+        while id != self.parent(id) {
+            self.set_parent(id, self.parent(self.parent(id)));
+            id = self.parent(id);
         }
+        id
     }
 
-    pub fn merge(&mut self, a: ClassId, b: ClassId) -> ClassId {
-        let fa = self.find(a);
-        let fb = self.find(b);
-        self.vec[fa.0 as usize] = fb;
-        fb
+    #[inline]
+    fn parent(&self, id: ClassId) -> ClassId {
+        self.vec[id.0 as usize]
+    }
+
+    #[inline]
+    fn set_parent(&mut self, id: ClassId, parent: ClassId) {
+        self.vec[id.0 as usize] = parent;
+    }
+
+    pub fn merge(&mut self, mut x: ClassId, mut y: ClassId) -> ClassId {
+        while self.parent(x) != self.parent(y) {
+            if self.parent(x) > self.parent(y) {
+                if x == self.parent(x) {
+                    self.set_parent(x, self.parent(y));
+                    break;
+                }
+                let z = self.parent(x);
+                self.set_parent(x, self.parent(y));
+                x = z;
+            } else {
+                if y == self.parent(y) {
+                    self.set_parent(y, self.parent(x));
+                    break;
+                }
+                let z = self.parent(y);
+                self.set_parent(y, self.parent(x));
+                y = z;
+            }
+        }
+        self.parent(x)
     }
 }
 
@@ -46,7 +69,7 @@ mod tests {
 
     #[test]
     #[cfg_attr(miri, ignore)]
-    fn union_find() {
+    fn simple_uf() {
         let mut uf = UnionFind::new();
         let x = uf.makeset();
         let y = uf.makeset();
@@ -57,12 +80,40 @@ mod tests {
         assert_eq!(uf.find(x), x);
         assert_eq!(uf.find(y), y);
         assert_eq!(uf.find(z), z);
-        uf.merge(x, y);
+        assert_eq!(uf.merge(x, y), x);
         assert_eq!(uf.find(x), uf.find(y));
         assert_ne!(uf.find(x), uf.find(z));
-        uf.merge(x, z);
+        assert_eq!(uf.merge(x, z), x);
         assert_eq!(uf.find(x), uf.find(z));
         assert_eq!(uf.find(y), uf.find(z));
         assert_eq!(uf.find(y), uf.find(x));
+    }
+
+    #[test]
+    #[cfg_attr(miri, ignore)]
+    fn complex_uf() {
+        let mut uf = UnionFind::new();
+        let mut ids = vec![];
+        for _ in 0..1000000 {
+            ids.push(uf.makeset());
+        }
+        for i in 0..999999 {
+            assert_ne!(uf.find(ids[i]), uf.find(ids[i + 1]));
+        }
+        for i in 0..500000 {
+            assert_eq!(uf.merge(ids[2 * i], ids[2 * i + 1]), ids[2 * i]);
+        }
+        for i in 0..500000 {
+            assert_eq!(uf.find(ids[2 * i]), uf.find(ids[2 * i + 1]));
+            if i < 499999 {
+                assert_ne!(uf.find(ids[2 * i]), uf.find(ids[2 * i + 2]));
+            }
+        }
+        for i in 0..499999 {
+            assert_eq!(uf.merge(ids[2 * i], ids[2 * i + 2]), ids[0]);
+        }
+        for i in 0..999999 {
+            assert_eq!(uf.find(ids[i]), uf.find(ids[999999]));
+        }
     }
 }
