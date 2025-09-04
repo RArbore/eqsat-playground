3d6942b3686890e4cb21a91dacb8ae4f0d072995
Russel Arbore <russel.jma@gmail.com>
2025-09-04 12:28:59 -0700

    Refactor abstract interpretation to be a little less gross

--- imp/src/ai.rs
+++ imp/src/ai.rs
@@ -1,4 +1,3 @@
-use core::cell::Cell;
 use std::collections::BTreeMap;
 
 use util::interner::{IdentifierId, StringInterner};
@@ -6,26 +5,6 @@ use util::union_find::ClassId;
 
 use crate::ast::{BlockAST, ExpressionAST, FunctionAST, ProgramAST, StatementAST};
 use crate::ssa::{Graph, Term};
-
-struct ControlLinker {
-    last: Cell<ClassId>,
-}
-
-impl ControlLinker {
-    fn new(id: ClassId) -> Self {
-        Self {
-            last: Cell::new(id),
-        }
-    }
-
-    fn get(&self) -> ClassId {
-        self.last.get()
-    }
-
-    fn set(&self, id: ClassId) {
-        self.last.set(id);
-    }
-}
 
 pub fn abstract_interpret(program: &ProgramAST<'_>, interner: &mut StringInterner) -> Vec<Graph> {
     let mut graphs = vec![];
@@ -37,13 +16,49 @@ pub fn abstract_interpret(program: &ProgramAST<'_>, interner: &mut StringInterne
     graphs
 }
 
-type AbstractStore = BTreeMap<IdentifierId, ClassId>;
+#[derive(Clone)]
+struct AbstractState {
+    vars: BTreeMap<IdentifierId, ClassId>,
+    pred: ClassId,
+}
+
+impl AbstractState {
+    fn new(start: ClassId) -> Self {
+        Self {
+            vars: BTreeMap::new(),
+            pred: start,
+        }
+    }
+
+    fn insert(&mut self, iden: IdentifierId, value: ClassId) -> Option<ClassId> {
+        self.vars.insert(iden, value)
+    }
+
+    fn get(&self, iden: &IdentifierId) -> Option<&ClassId> {
+        self.vars.get(iden)
+    }
+
+    fn iter(&self) -> impl Iterator<Item = (&IdentifierId, &ClassId)> {
+        self.vars.iter()
+    }
+
+    fn iter_mut(&mut self) -> impl Iterator<Item = (&IdentifierId, &mut ClassId)> {
+        self.vars.iter_mut()
+    }
+
+    fn pred(&self) -> ClassId {
+        self.pred
+    }
+
+    fn set_pred(&mut self, pred: ClassId) {
+        self.pred = pred;
+    }
+}
 
 fn ai_func(func: &FunctionAST<'_>, graph: &mut Graph) {
     let root = graph.makeset();
     graph.insert(Term::Start { root });
-    let ctrl = ControlLinker::new(root);
-    let mut s = AbstractStore::new();
+    let mut s = AbstractState::new(root);
     for (idx, iden) in func.params.as_ref().into_iter().enumerate() {
         let root = graph.makeset();
         graph.insert(Term::Param {
@@ -52,32 +67,22 @@ fn ai_func(func: &FunctionAST<'_>, graph: &mut Graph) {
         });
         s.insert(*iden, graph.find(root));
     }
-    ai_block(&func.block, &s, graph, &ctrl);
+    ai_block(&func.block, &s, graph);
 }
 
-fn ai_block(
-    block: &BlockAST<'_>,
-    in_s: &AbstractStore,
-    graph: &mut Graph,
-    ctrl: &ControlLinker,
-) -> AbstractStore {
+fn ai_block(block: &BlockAST<'_>, in_s: &AbstractState, graph: &mut Graph) -> AbstractState {
     let mut s = in_s.clone();
     for stmt in block.stmts.as_ref() {
-        s = ai_stmt(stmt, &s, graph, ctrl);
+        s = ai_stmt(stmt, &s, graph);
     }
     s
 }
 
-fn ai_stmt(
-    stmt: &StatementAST<'_>,
-    in_s: &AbstractStore,
-    graph: &mut Graph,
-    ctrl: &ControlLinker,
-) -> AbstractStore {
+fn ai_stmt(stmt: &StatementAST<'_>, in_s: &AbstractState, graph: &mut Graph) -> AbstractState {
     let merge_down =
-        |region: ClassId, lhs_s: &AbstractStore, rhs_s: &AbstractStore, graph: &mut Graph| {
-            let mut merged_s = AbstractStore::new();
-            for (lhs_iden, lhs_expr) in lhs_s {
+        |region: ClassId, lhs_s: &AbstractState, rhs_s: &AbstractState, graph: &mut Graph| {
+            let mut merged_s = AbstractState::new(region);
+            for (lhs_iden, lhs_expr) in lhs_s.iter() {
                 if let Some(rhs_expr) = rhs_s.get(&lhs_iden) {
                     if *lhs_expr == *rhs_expr {
                         merged_s.insert(*lhs_iden, *lhs_expr);
@@ -98,150 +103,129 @@ fn ai_stmt(
 
     use StatementAST::*;
     match stmt {
-        Block(block) => ai_block(block, in_s, graph, ctrl),
+        Block(block) => ai_block(block, in_s, graph),
         Assign(iden, expr) => {
-            let expr = ai_expr(expr, in_s, graph, ctrl);
+            let expr = ai_expr(expr, in_s, graph);
             let mut s = in_s.clone();
             s.insert(*iden, expr);
             s
         }
         IfElse(cond, lhs, rhs) => {
-            let cond = ai_expr(cond, in_s, graph, ctrl);
-            let pred = ctrl.get();
+            let cond = ai_expr(cond, in_s, graph);
+            let pred = in_s.pred();
             let mut root = graph.makeset();
             graph.insert(Term::Branch { pred, cond, root });
+
             root = graph.find(root);
-            let mut lhs_root = graph.makeset();
+            let lhs_root = graph.makeset();
             graph.insert(Term::ControlProj {
                 pred: root,
                 index: 1,
                 root: lhs_root,
             });
-            let mut rhs_root = graph.makeset();
+            let mut lhs_s = in_s.clone();
+            lhs_s.set_pred(lhs_root);
+            let lhs_s = ai_block(lhs, &lhs_s, graph);
+
+            let rhs_root = graph.makeset();
             graph.insert(Term::ControlProj {
                 pred: root,
                 index: 0,
                 root: rhs_root,
             });
-
-            ctrl.set(lhs_root);
-            let lhs_s = ai_block(lhs, in_s, graph, ctrl);
-            lhs_root = ctrl.get();
-            ctrl.set(rhs_root);
+            let mut rhs_s = in_s.clone();
+            rhs_s.set_pred(rhs_root);
             let rhs_s = if let Some(rhs) = rhs {
-                &ai_block(rhs, in_s, graph, ctrl)
+                &ai_block(rhs, &rhs_s, graph)
             } else {
-                in_s
+                &rhs_s
             };
-            rhs_root = ctrl.get();
+
             let root = graph.makeset();
             graph.insert(Term::Region {
-                lhs: lhs_root,
-                rhs: rhs_root,
+                lhs: lhs_s.pred(),
+                rhs: rhs_s.pred(),
                 root,
             });
-            ctrl.set(root);
             merge_down(root, &lhs_s, &rhs_s, graph)
         }
         While(cond, body) => {
-            let loop_body_region = graph.makeset();
-            let loop_exit_region = graph.makeset();
-            let loop_body_branch = graph.makeset();
-            let loop_entry_proj = graph.makeset();
-            let entry_pred = ctrl.get();
-            ctrl.set(loop_entry_proj);
-            let original_cond_expr = ai_expr(cond, in_s, graph, ctrl);
-            ctrl.set(loop_body_region);
-            let mut after_body_s = ai_block(body, in_s, graph, ctrl);
-            let mut last_concrete_expr = AbstractStore::new();
-            let mut static_phis = AbstractStore::new();
+            let loop_cond_region = graph.makeset();
+            let loop_cond_branch = graph.makeset();
+            let loop_cond_proj_true = graph.makeset();
+            let loop_cond_proj_false = graph.makeset();
+
+            let mut prev_state = None;
+            let mut static_phis = BTreeMap::new();
+            let mut last_exprs = BTreeMap::new();
             let mut changed = true;
-            let (loop_body_bottom, cond_expr) = loop {
-                let mut before_body_s = merge_down(loop_body_region, in_s, &after_body_s, graph);
-                if !changed {
-                    for (iden, static_phi) in static_phis {
-                        graph.merge(static_phi, last_concrete_expr[&iden]);
-                    }
-                    ctrl.set(loop_body_region);
-                    after_body_s = ai_block(body, &before_body_s, graph, ctrl);
-                    break (ctrl.get(), ai_expr(cond, &after_body_s, graph, ctrl));
-                } else {
+            let (cond, bottom_pred, break_s) = loop {
+                let mut top_s = if let Some(ref prev_state) = prev_state {
                     changed = false;
-                    for (iden, new_expr) in before_body_s.iter_mut() {
-                        if let Some(old_expr) = after_body_s.get(iden)
-                            && *new_expr != *old_expr
+                    let mut merged_s = merge_down(loop_cond_region, in_s, &prev_state, graph);
+                    for (iden, merged_expr) in merged_s.iter_mut() {
+                        if let Some(old_expr) = prev_state.get(iden)
+                            && *merged_expr != *old_expr
                         {
-                            if let Some(concrete_expr) = last_concrete_expr.insert(*iden, *new_expr)
-                            {
-                                changed = concrete_expr != *new_expr || changed;
-                            } else {
+                            if let Some(last_expr) = last_exprs.insert(*iden, *merged_expr) {
+                                changed = changed || last_expr != *merged_expr;
+                            }
+
+                            if !static_phis.contains_key(iden) {
                                 let static_phi = graph.makeset();
-                                *new_expr = static_phi;
                                 static_phis.insert(*iden, static_phi);
                                 changed = true;
                             }
-                        }
-                        if let Some(static_phi) = static_phis.get(iden) {
-                            *new_expr = *static_phi;
+                            *merged_expr = static_phis[&iden];
                         }
                     }
-                    ctrl.set(loop_body_region);
-                    after_body_s = ai_block(body, &before_body_s, graph, ctrl);
-                    ai_expr(cond, &after_body_s, graph, ctrl);
+                    merged_s
+                } else {
+                    in_s.clone()
+                };
+
+                if !changed {
+                    for (iden, static_phi) in static_phis {
+                        graph.merge(static_phi, last_exprs[&iden]);
+                    }
+                    top_s.set_pred(loop_cond_branch);
+                    let cond = ai_expr(cond, &top_s, graph);
+                    top_s.set_pred(loop_cond_proj_false);
+                    break (cond, prev_state.unwrap().pred(), top_s);
                 }
+
+                top_s.set_pred(loop_cond_proj_true);
+                let bottom_s = ai_block(body, &top_s, graph);
+                prev_state = Some(bottom_s);
             };
-            let loop_entry_branch = graph.makeset();
-            graph.insert(Term::Branch {
-                pred: entry_pred,
-                cond: original_cond_expr,
-                root: loop_entry_branch,
-            });
-            let loop_skip_proj = graph.makeset();
-            graph.insert(Term::ControlProj {
-                pred: loop_entry_branch,
-                index: 0,
-                root: loop_skip_proj,
-            });
-            graph.insert(Term::ControlProj {
-                pred: loop_entry_branch,
-                index: 1,
-                root: loop_entry_proj,
+
+            graph.insert(Term::Region {
+                lhs: in_s.pred(),
+                rhs: bottom_pred,
+                root: loop_cond_region,
             });
             graph.insert(Term::Branch {
-                pred: loop_body_bottom,
-                cond: cond_expr,
-                root: loop_body_branch,
+                pred: loop_cond_region,
+                cond,
+                root: loop_cond_branch,
             });
-            let loop_body_done_proj = graph.makeset();
             graph.insert(Term::ControlProj {
-                pred: loop_body_branch,
+                pred: loop_cond_branch,
                 index: 0,
-                root: loop_body_done_proj,
+                root: loop_cond_proj_false,
             });
-            let loop_body_continue_proj = graph.makeset();
             graph.insert(Term::ControlProj {
-                pred: loop_body_branch,
+                pred: loop_cond_branch,
                 index: 1,
-                root: loop_body_continue_proj,
+                root: loop_cond_proj_true,
             });
-            graph.insert(Term::Region {
-                lhs: loop_entry_proj,
-                rhs: loop_body_continue_proj,
-                root: loop_body_region,
-            });
-            graph.insert(Term::Region {
-                lhs: loop_skip_proj,
-                rhs: loop_body_done_proj,
-                root: loop_exit_region,
-            });
-            ctrl.set(loop_exit_region);
-            merge_down(loop_exit_region, in_s, &after_body_s, graph)
+            break_s
         }
         Return(expr) => {
-            let value = ai_expr(expr, in_s, graph, ctrl);
+            let value = ai_expr(expr, in_s, graph);
             let root = graph.makeset();
             graph.insert(Term::Finish {
-                pred: ctrl.get(),
+                pred: in_s.pred(),
                 value,
                 root,
             });
@@ -250,12 +234,7 @@ fn ai_stmt(
     }
 }
 
-fn ai_expr(
-    expr: &ExpressionAST<'_>,
-    in_s: &AbstractStore,
-    graph: &mut Graph,
-    ctrl: &ControlLinker,
-) -> ClassId {
+fn ai_expr(expr: &ExpressionAST<'_>, in_s: &AbstractState, graph: &mut Graph) -> ClassId {
     use ExpressionAST::*;
     match expr {
         NumberLiteral(value) => {
@@ -268,8 +247,8 @@ fn ai_expr(
         }
         Variable(iden) => *in_s.get(iden).unwrap(),
         Add(lhs, rhs) => {
-            let lhs = ai_expr(lhs, in_s, graph, ctrl);
-            let rhs = ai_expr(rhs, in_s, graph, ctrl);
+            let lhs = ai_expr(lhs, in_s, graph);
+            let rhs = ai_expr(rhs, in_s, graph);
             let root = graph.makeset();
             graph.insert(Term::Add { lhs, rhs, root });
             graph.find(root)
@@ -304,7 +283,7 @@ mod tests {
         graphs[0].rebuild();
         assert_eq!(
             graphs[0].dump(&interner),
-            "cons([4294967295]) -> [6]\nparam([0]) -> [1]\nstart([]) -> [0]\nregion([5, 24]) -> [2]\nregion([22, 23]) -> [3]\nbranch([0, 1]) -> [21]\nbranch([2, 11]) -> [4]\nπ([21, 0]) -> [22]\nπ([21, 1]) -> [5]\nπ([4, 0]) -> [23]\nπ([4, 1]) -> [24]\nfinish([3, 25]) -> [26]\nϕ([2, 1, 7]) -> [8]\nϕ([2, 1, 11]) -> [9]\nϕ([3, 1, 11]) -> [25]\n+([1, 6]) -> [7]\n+([9, 6]) -> [11]\n",
+            "cons([4294967295]) -> [6]\nparam([0]) -> [1]\nstart([]) -> [0]\nregion([0, 4]) -> [2]\nbranch([2, 9]) -> [3]\nπ([3, 0]) -> [5]\nπ([3, 1]) -> [4]\nfinish([5, 9]) -> [16]\nϕ([2, 1, 7]) -> [8]\nϕ([2, 1, 11]) -> [9]\n+([1, 6]) -> [7]\n+([9, 6]) -> [11]\n",
         );
     }
 }
